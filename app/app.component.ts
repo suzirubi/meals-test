e6c9ec92bb55ff1dd1190e0179cc6340dda2fee3
SuziRubino <suzi@suzirubi.com>
2017-01-19 18:25:25 -0800

    create initial files and change gulpfile to reflect a scss folder

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <h1>MEALS I ATE</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+
+}
