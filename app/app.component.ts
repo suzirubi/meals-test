16e5c1b79fbfd7936a7401680590449e7e5b02a2
SuziRubino <suzi@suzirubi.com>
2017-01-19 20:13:50 -0800

    add fonts and scss basic styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="container">
       <h1>MEALS I ATE</h1>
+      <h2>...or at least I wish I ate</h2>
     </div>
   `
 })
--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('serve', ['build'], function() {
   });
   gulp.watch(['resources/js/*.js'], ['jsBuild']); // vanilla js changes, reload.
   gulp.watch(['*.html'], ['htmlBuild']); // html changes, reload.
-  gulp.watch(['resources/styles/*.css', 'resources/styles/scss/*.scss'], ['cssBuild']);      gulp.watch(['app/*.ts'], ['tsBuild']); // typescript files change, compile then reload.
+  gulp.watch(['resources/styles/*.css', 'resources/styles/*.scss'], ['cssBuild']);      gulp.watch(['app/*.ts'], ['tsBuild']); // typescript files change, compile then reload.
 });
 
 gulp.task('jsBuild', function(){
